aabb04f3244a456a87d67d2d577cdd0cd1c3d005
dankozsolti <dankozsolti@freemail.hu>
2020-11-18 23:43:41 +0100

    started working on the cart bar

--- src/main/java/hu/csapatnev/webshop/controller/GlobalController.java
+++ src/main/java/hu/csapatnev/webshop/controller/GlobalController.java
@@ -1,9 +1,15 @@
 package hu.csapatnev.webshop.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import hu.csapatnev.webshop.Config;
+import hu.csapatnev.webshop.session.CartItem;
 
 @ControllerAdvice
 public class GlobalController {
@@ -16,4 +22,13 @@ public class GlobalController {
     public String siteName() {
         return Config.getInstance().getProperties().getProperty("site.name");
     }
+	
+	@SuppressWarnings("unchecked")
+	@ModelAttribute("cartItems")
+	public List<CartItem> cartItems(HttpServletRequest request) {
+		List<CartItem> items = (List<CartItem>) request.getSession().getAttribute("cart");
+		if (items == null)
+			items = new ArrayList<CartItem>();
+		return items;
+	}
 }
